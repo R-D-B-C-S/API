3a1a6de0d7bc70726d2dc45875d2066ba779e89b
R-D-B-C-S <dee40@scarletmail.rutgers.edu>
2023-04-03 11:05:23 -0400

    current version

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "YOUR KEY HERE"
+
+# Geoapify API Key
+geoapify_key = "prj_test_pk_483120c747403662b1c29e1dc4ffac9a38a4568b"
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "93fd2e469176b77c021c75f2630a98c0"
+
+# Geoapify API Key
+geoapify_key = "YOUR KEY HERE"
